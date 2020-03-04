65aa99d1216094c6bd28f9d65c2cc2ec9cfd0852
vitalion <vitalion@localhost>
2020-03-04 11:31:39 +0300

    Task Two

---
+++ LineProgrammsOne/src/by/alehno/TaskTwo.java
@@ -0,0 +1,14 @@
+package by.alehno;
+
+public class TaskTwo {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=6;
+		int c=5;
+		double z;
+		z=(b+Math.sqrt(b*b+4*a*c))/(2*a)-a*a*a*c+(1/b);
+		System.out.println("z = "+z);
+	}
+
+}
