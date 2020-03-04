354425ef57f4bbee83efc938627130b933941e02
vitalion <vitalion@localhost>
2020-03-04 14:22:07 +0300

    Task Three

---
+++ LineProgrammsOne/src/by/alehno/TaskThree.java
@@ -0,0 +1,14 @@
+package by.alehno;
+//z=(Math.sin(x)+Math.cos(y))/(Math.cos(y)-Math.sin(x))*Math.tan(x*y);
+public class TaskThree {
+
+	public static void main(String[] args) {
+		int x=3;
+		int y=5;
+		double z;
+		z=(Math.sin(x)+Math.cos(y))/(Math.cos(y)-Math.sin(x))*Math.tan(x*y);
+		System.out.println("z = "+z);
+
+	}
+
+}
