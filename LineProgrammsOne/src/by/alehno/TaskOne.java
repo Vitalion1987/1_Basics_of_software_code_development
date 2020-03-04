e7734ebabdef11a1e2de88e77cbf0333cb92a3a7
vitalion <vitalion@localhost>
2020-03-04 11:30:48 +0300

    Task One

---
+++ LineProgrammsOne/src/by/alehno/TaskOne.java
@@ -0,0 +1,14 @@
+package by.alehno;
+//z = ((a-3)*b/2)+c;
+public class TaskOne {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=5;
+		int c=8;
+		int z;
+		z = ((a-3)*b/2)+c;
+		System.out.println("z = "+z);
+	}
+
+}
